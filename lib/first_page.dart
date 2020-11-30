dcc083fa65b751b5d67e0c65463189a5fe0d6d22
sbrow92 <31661654+sbrow92@users.noreply.github.com>
2020-11-29 20:01:45 -0800

    Added second button and msg2

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -8,6 +8,7 @@ class MyFirstPage extends StatefulWidget {
 class _MyFirstPageState extends State<MyFirstPage> {
   bool _enabled = false;
   String _msg1 = '';
+  String _msg2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -115,6 +116,37 @@ class _MyFirstPageState extends State<MyFirstPage> {
                     //The text of the button works separately from the button
                     //itself so we must update it when the switch is changed.
                     child: Text(_msg1),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    elevation: 8,
+                    textColor: Colors.red.shade100,
+                    color: Colors.lightBlueAccent,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.green.shade300,
+                    padding: EdgeInsets.all(20.0),
+                    //onPressed1 is run every time the build is run,
+                    //because of the ().
+                    //If _enabled is true onPressed1 will return
+                    //the address of an anonymous function that
+                    //will only run when the button is pressed.
+                    //At this time the button will appear and be enabled.
+                    //If _enabled is false onPressed1 will return
+                    //null and the button will disappear and be disabled.
+                    onPressed: onPressed1(),
+
+                    //If we call onPressed2 instead of onPressed1 the
+                    //anonymous function will run but the button
+                    //will NOT appear, THIS DOES NOT WORK.
+                    //onPressed: onPressed2(),
+
+                    //The text of the button works separately from the button
+                    //itself so we must update it when the switch is changed.
+                    child: Text(_msg2),
                   ),
                 ),
               ],
